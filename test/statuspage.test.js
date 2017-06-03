9cfffc71b0bba99709e4e7ffcab8b52e3b7ea440
Dionisio Perez-Mavrogenis <dperezmavrogenis@lastmilelink.com>
2017-06-03 12:48:01 +0100

    fixed constructor issue

--- test/statuspage.test.js
+++ test/statuspage.test.js
@@ -1,37 +1,40 @@
-const expect = require("expect");
-const axios = require("axios");
-// const sinon = require("sinon");
-const uuid = require("uuid");
+const expect = require('expect');
+const axios = require('axios');
+// const sinon = require('sinon');
+const uuid = require('uuid');
 // var moment = require('moment');
-var StatusPage = require("../index.js");
-const metric_id = "07fbhggxqj6l";
+var {StatusPage} = require('../index.js');
+const metric_id = '07fbhggxqj6l';
 
-describe("StatusPage", () => {
-  it("Should exist", ()=>{
+describe('StatusPage', () => {
+  it('Should exist', ()=>{
     expect(
       StatusPage
     ).toExist();
 
-    expect(
-      Object.keys(StatusPage).length
-    ).toBeGreaterThan(0)
+    // expect(
+    //   Object.keys(StatusPage).length
+    // ).toBeGreaterThan(0)
   });
 
-  // it("Should return POST path", () => {
-  //   var s = new StatusPage();
-  //   expect(s.generateMetricPostPath("456"))
-  //   .toEqual("/v1/pages/swxb42jtqxmx/metrics/456/data.json")
-  // });
+  it('Should return POST path', () => {
+    const pageId = 'MyTestPageId123'
+    const s = new StatusPage(undefined, 'MyTestPageId123');
+    expect(
+      s.generateMetricPostPath('456')
+    )
+    .toEqual(`/v1/pages/${pageId}/metrics/456/data.json`)
+  });
   //
-  // it("Should generate the request string", () => {
+  // it('Should generate the request string', () => {
   //   var s = new StatusPage();
-  //   var res = s.generateRequestParameters("31337");
+  //   var res = s.generateRequestParameters('31337');
   //
   //   expect(/^data\[timestamp\]=[0-9]+&data\[value\]=31337$/.test(res))
   //   .toBe(true);
   // });
   //
-  // it("Should successfully post for valid page_id and metric_id", () => {
+  // it('Should successfully post for valid page_id and metric_id', () => {
   //   var s = new StatusPage();
   //   var instance = axios.create();
   //   var time = Date.now();
@@ -47,37 +50,37 @@ describe("StatusPage", () => {
   //   s.updateMetric(metric_id, time);
   // });
   //
-  // it("Should not post for invalid page_id and metric_id", (done) => {
+  // it('Should not post for invalid page_id and metric_id', (done) => {
   //   var s = new StatusPage();
-  //   s.updateMetric("", 1339)
+  //   s.updateMetric('', 1339)
   //   .catch((e) => {
   //     expect(e).toNotBe(undefined);
   //     done();
   //   });
   // });
   //
-  // it("Should generate API base path", () => {
+  // it('Should generate API base path', () => {
   //   var s = new StatusPage();
-  //   s.page_id = "123";
-  //   expect(s.generatePagePath()).toEqual("/v1/pages/123")
+  //   s.page_id = '123';
+  //   expect(s.generatePagePath()).toEqual('/v1/pages/123')
   // });
   //
-  // it("Shoud generate component status payload", () => {
+  // it('Shoud generate component status payload', () => {
   //   var s = new StatusPage();
-  //   s.page_id = "123";
-  //   expect(s.generateComponentStatusPayload("stopped"))
-  //   .toEqual("component[status]=stopped");
+  //   s.page_id = '123';
+  //   expect(s.generateComponentStatusPayload('stopped'))
+  //   .toEqual('component[status]=stopped');
   // });
   //
-  // it("Should generate component path", () => {
+  // it('Should generate component path', () => {
   //   var s = new StatusPage();
-  //   var res = s.generateComponentPath("asdf");
+  //   var res = s.generateComponentPath('asdf');
   //
   //   expect(/^\/v1\/pages\/[a-z0-9]+\/components\/asdf.json$/.test(res))
   //   .toBe(true);
   // });
   //
-  // it("Should get components", (done) => {
+  // it('Should get components', (done) => {
   //   var s = new StatusPage();
   //   s.getComponents((data) => {
   //     expect(data).toNotBe(undefined);
@@ -85,11 +88,11 @@ describe("StatusPage", () => {
   //   });
   // });
   //
-  // it("Should fail to get components", () => {
+  // it('Should fail to get components', () => {
   //   var instance = axios.create();
-  //   sinon.stub(instance, "get", function(){
+  //   sinon.stub(instance, 'get', function(){
   //     return new Promise(function(acc, rej){
-  //       rej("1337-3rr0r");
+  //       rej('1337-3rr0r');
   //     });
   //   });
   //
@@ -101,37 +104,37 @@ describe("StatusPage", () => {
   //   expect(spy).toNotHaveBeenCalled();
   // });
   //
-  // it("Should create a component", () => {
+  // it('Should create a component', () => {
   //   var instance = axios.create();
-  //   sinon.stub(instance, "post", function(a, b){
-  //     expect(a).toEqual("/v1/pages/page_id/components.json");
-  //     expect(b).toEqual("component[name]=1337-c0mp0nent");
+  //   sinon.stub(instance, 'post', function(a, b){
+  //     expect(a).toEqual('/v1/pages/page_id/components.json');
+  //     expect(b).toEqual('component[name]=1337-c0mp0nent');
   //   });
   //
   //   var s = new StatusPage();
-  //   s.page_id = "page_id";
+  //   s.page_id = 'page_id';
   //   s.instance = instance;
   //
-  //   s.createComponent("1337-c0mp0nent");
+  //   s.createComponent('1337-c0mp0nent');
   //
   // });
   //
-  // it("Should generate an appropriate component name payload", () => {
+  // it('Should generate an appropriate component name payload', () => {
   //   var s = new StatusPage();
-  //   var res = s.generateCreateComponentPayload("potatoes-component");
-  //   expect(res).toEqual("component[name]=potatoes-component");
+  //   var res = s.generateCreateComponentPayload('potatoes-component');
+  //   expect(res).toEqual('component[name]=potatoes-component');
   // });
   //
-  // it("Should update and toggle components correctly", (done) =>  {
+  // it('Should update and toggle components correctly', (done) =>  {
   //   var s = new StatusPage();
-  //   const testing_component_id = "6jgmt1plscnx";
-  //   s.updateComponentState(testing_component_id, "partial_outage", undefined)
+  //   const testing_component_id = '6jgmt1plscnx';
+  //   s.updateComponentState(testing_component_id, 'partial_outage', undefined)
   //   .then((a) => {
   //     s.getComponents((components) => {
   //       var arr = components.filter(({id}) => id === testing_component_id);
   //       expect(arr.length).toBe(1);
-  //       expect(arr[0].status).toEqual("partial_outage");
-  //       s.updateComponentState(testing_component_id, "operational", undefined)
+  //       expect(arr[0].status).toEqual('partial_outage');
+  //       s.updateComponentState(testing_component_id, 'operational', undefined)
   //       .then((d) => {
   //         done();
   //       })
